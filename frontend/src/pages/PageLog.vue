734b46107b83fda3339d0073416460259155cf2d
libgcc <uname@foxmail.com>
2018-06-21 17:13:37 +0800

    添加日志页面

--- frontend/src/pages/PageLog.vue
+++ frontend/src/pages/PageLog.vue
@@ -1,38 +1,168 @@
 <template>
-    <section>
-        <div>{{name|capitalize}}</div>
-        <img src="@/assets/fire.png" alt="">
-    </section>
+    <article class="wrapper-box">
+        <header>
+            <div>
+                <span>开始时间:</span>
+                <ZlDatePicker v-model="startDate"></ZlDatePicker>
+            </div>
+            <div>
+                <span>结束时间:</span>
+                <ZlDatePicker v-model="endDate"></ZlDatePicker>
+            </div>
+            <button type="text"><i class="iconfont icon-search"></i></button>
+            <button type="text"><i class="iconfont icon-send"></i></button>
+        </header>
+        <section>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th v-for="field in fields">{{field.caption}}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="i in 20">
+                            <td v-for="field in fields"><span><br> </span></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <footer>
+                <div>
+                    <button type="text">
+                        <i class="iconfont icon-angle-double-left"></i>
+                      </button>
+                    <button type="text">
+                        <i class="iconfont icon-return"></i>
+                      </button>
+                    <input value="1">
+                    <button type="text">
+                        <i class="iconfont icon-enter"></i>
+                      </button>
+                    <button type="text">
+                        <i class="iconfont icon-angle-double-right"></i>
+                      </button>
+                    <span> 共1页 0条</span>
+                </div>
+            </footer>
+        </section>
+    </article>
 </template>
 
 <script>
+    import ZlDatePicker from "../components/ZlDatePicker";
     export default {
+        components: {
+            ZlDatePicker,
+        },
         data() {
             return {
-                name: ""
+                startDate: null,
+                endDate: null,
+                fields: [{
+                        name: "wire_name",
+                        caption: "序号"
+                    },
+                    {
+                        name: "fault_handle",
+                        caption: "时间"
+                    },
+                    {
+                        name: "fault_level",
+                        caption: "类型"
+                    },
+                    {
+                        name: "fault_level",
+                        caption: "日志内容"
+                    },
+                ]
             };
         },
-        filters: {
-            capitalize(val) {
-                if (!val) {
-                    return "";
-                }
-                val = val.toString();
-                return val.charAt(0).toUpperCase() + val.slice(1);
-            }
-        }
     };
 </script>
 
 <style scoped>
-section{
-    background-color: #ccc;
-}
-div{
-    width: 30px;
-    height: 262px;
-    background: linear-gradient(to top ,#000000 ,#ff7f00,#ffff7f,#fffff2);
-    float: left;
-    margin: 0 20px;
-}
+    header {
+        height: 40px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 10px;
+        background-color: #132d48;
+    }
+    
+    header>div {
+        margin-right: 20px;
+    }
+    
+    header select {
+        width: 200px;
+    }
+    
+    section {
+        position: relative;
+        height: calc(100% - 60px);
+        background-color: #ccc;
+        background-color: #132d48;
+        border: solid 1px #406985;
+        overflow: hidden;
+    }
+    
+    footer {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        top: auto;
+        right: 0;
+        height: 30px;
+        line-height: 30px;
+        background-color: #133551;
+        padding-left: 10px;
+    }
+    
+    table {
+        width: 100%;
+        border-collapse: collapse;
+    }
+    
+    td,
+    th,
+    table {
+        border: 1px solid #2b4f69;
+        font-weight: normal;
+    }
+    
+    thead {
+        background-color: #06192a;
+        line-height: 28px;
+    }
+    
+    tbody {
+        line-height: 26px;
+        text-align: center;
+    }
+    
+    footer div {
+        margin-right: 50px;
+        float: left;
+    }
+    
+    footer input {
+        width: 16px;
+        text-align: center;
+    }
+    
+    .iconfont {
+        color: #fff;
+        font-size: 20px;
+    }
+    
+    .wave {
+        position: absolute;
+        top: 50px;
+        left: 50px;
+        height: 300px;
+        width: 400px;
+        background-color: #fff;
+    }
 </style>
